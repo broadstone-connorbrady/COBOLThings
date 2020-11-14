a9f806f820841d55761e2c39e60abe0ad43e6080
Connor Brady <connorbrady@codox.eu>
2020-11-14 13:56:42 +0000

    ADDED Value assignments
    COMPUTE + Math
    
    Todo: Sum negative numbers + remove trailing zeros

--- main.cob
+++ main.cob
@@ -12,11 +12,12 @@ DATA DIVISION.
 FILE SECTION.
 
 *> PROGRAMING VARIABLES
+*> COBOL Does not enforce types - The programmer does this
 WORKING-STORAGE SECTION.
-01 UserName PIC X(30) VALUE "CodoxConnor".
-01 UserPreBalance PIC 9 VALUE ZEROS.
-01 UserTransactionValue PIC 9 VALUE ZEROS.
-01 UserNewBalance PIC 99 VALUES 0.
+01 UserName PIC X(30).
+01 UserPreBalance PIC 9(18) VALUE ZEROS.
+01 UserTransactionValue PIC 9(18) VALUE ZEROS.
+01 UserNewBalance PIC 9(18) VALUES 0.
 01 NINumber PIC X(10).
 
 01 TaxRate CONSTANT AS 20.
@@ -32,6 +33,27 @@ PROCEDURE DIVISION.
 
 DISPLAY "Name " WITH NO ADVANCING
 ACCEPT UserName
-DISPLAY "Editing " UserName
+
+DISPLAY "NI Number: " WITH NO ADVANCING
+ACCEPT NINumber
+
+DISPLAY "Editing " UserName 
+DISPLAY "NI Number: " NINumber
+
+DISPLAY "Enter user's current balance: " WITH NO ADVANCING
+ACCEPT UserPreBalance
+
+DISPLAY "Enter new transaction amount: " WITH NO ADVANCING
+ACCEPT UserTransactionValue
+
+DISPLAY "PreBalance: " UserPreBalance
+DISPLAY "Transaction: " UserTransactionValue 
+
+COMPUTE UserNewBalance = UserPreBalance + UserTransactionValue
+
+DISPLAY "User new balance = " UserNewBalance
 
 STOP RUN.
+
+*> OTHER NOTES
+*> WITH NO ADVANCING - No newline on display
